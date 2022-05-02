818318af25286da1b39809f2417965169feba9a1
Dylan FAURE <faure78@localhost.localdomain>
2022-05-02 22:25:17 +0200

    [ADD] creation page with full source code in fullSplitSpider

--- scraping/python/dogPython/dogPython/spiders/quotes_spider.py
+++ scraping/python/dogPython/dogPython/spiders/quotes_spider.py
@@ -27,6 +27,13 @@ class fullSplitSpider(scrapy.Spider):
     def parse(self, response):
         page = response.url.split("/")[-2]
 
+        # Création du fichier qui contient tout
+        filename = f'{self.name}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+
+
         # Rename du nom de la file a créer en "file-img.html"
         filename = f'{self.name}-img.html'
         # Récupération d'un tableau contenant toutes les balises img
@@ -34,61 +41,61 @@ class fullSplitSpider(scrapy.Spider):
         # Ecriture du tableau dans le fichier "file-img.html"
         writeStrInFile(filename, tabImg)
         # Ecriture dans les logs que la page a bien été sauvegarder
-        self.log(f'File {filename}-img saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise div
         filename = f'{self.name}-div.html'
         tabDiv = response.xpath(f"//div").getall()
         writeStrInFile(filename, tabDiv)
-        self.log(f'File {filename}-div saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise p
         filename = f'{self.name}-p.html'
         tabP = response.xpath(f"//p").getall()
         writeStrInFile(filename, tabP)
-        self.log(f'File {filename}-p saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise form
         filename = f'{self.name}-form.html'
         tabForm = response.xpath(f"//form").getall()
         writeStrInFile(filename, tabForm)
-        self.log(f'File {filename}-form saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise table
         filename = f'{self.name}-table.html'
         tabTable = response.xpath(f"//table").getall()
         writeStrInFile(filename, tabTable)
-        self.log(f'File {filename}-table saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise ul
         filename = f'{self.name}-ul.html'
         tabUl = response.xpath(f"//ul").getall()
         writeStrInFile(filename, tabUl)
-        self.log(f'File {filename}-ul saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise input
         filename = f'{self.name}-input.html'
         tabInput = response.xpath(f"//input").getall()
         writeStrInFile(filename, tabInput)
-        self.log(f'File {filename}-input saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise label
         filename = f'{self.name}-label.html'
         tabLabel = response.xpath(f"//label").getall()
         writeStrInFile(filename, tabLabel)
-        self.log(f'File {filename}-label saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise span
         filename = f'{self.name}-span.html'
         tabSpan = response.xpath(f"//span").getall()
         writeStrInFile(filename, tabSpan)
-        self.log(f'File {filename}-span saved')
+        self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise button
         filename = f'{self.name}-button.html'
         tabButton = response.xpath(f"//button").getall()
         writeStrInFile(filename, tabButton)
-        self.log(f'File {filename}-button saved')
+        self.log(f'File {filename} saved')
 
 # class tabiSpider(scrapy.Spider):
 #     name = "tabi"
