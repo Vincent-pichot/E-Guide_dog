7365b1f1a5a77afd442a1fdd4c91fccb0971b7e4
Chloé CHAUVIN <chloe.chauvin@epitech.eu>
2022-05-04 00:15:17 +0200

    [UPD] delete all html comments

--- scraping/python/dogPython/dogPython/spiders/quotes_spider.py
+++ scraping/python/dogPython/dogPython/spiders/quotes_spider.py
@@ -26,23 +26,25 @@ class fullSplitSpider(scrapy.Spider):
     
     def removeComments(self, tab):
         cleanTab = []
+        comment = False
 
         for line in tab:
-            posStart = line.find('<!--')
-            posEnd = -1
-            ##print("start = " + str(posStart))
+            posStart = line.find('<!--') # trouve le début du commentaire
+            posEnd = line.find('-->') # trouve la fin du commentaire
             if posStart != -1:
-                # check if the comment is one line only
-                posEnd = line.find('-->')
-                ##print("end = " + str(posEnd))
+                comment = True
                 if posEnd != -1:
                     rep = line[posStart:posEnd + 3]
-                    ##print(rep)
-                    line = line.replace(rep, "")
-
-                # removes all the lines until the end of comment
+                    line = line.replace(rep, "") # supprime un commentaire d'une seule ligne
+                    comment = False
+                else:
+                    line = line.replace(line[posStart:], "") # supprime le début du commentaire sur plusieurs lignes
+            elif posEnd != -1 and comment:
+                line = line.replace(line[:posEnd + 3], "") # supprime la fin du commentaire sur plusieurs lignes
+                comment = False
+            elif comment:
+                line = line.replace(line, "") # supprime les lignes du commentaire sur plusieurs lignes
             cleanTab.append(line)
-            # no comment to remove
         return cleanTab
 
     def parse(self, response):
@@ -54,9 +56,6 @@ class fullSplitSpider(scrapy.Spider):
             content = response.body.decode("utf-8")
             tab = content.split("\n")
             clean = self.removeComments(tab)
-            #for line in clean:
-            #    print(line)
-            #f.write(response.body)
             writeStrInFile(filename, clean)
         self.log(f'Saved file {filename}')
 
@@ -65,64 +64,96 @@ class fullSplitSpider(scrapy.Spider):
         filename = f'{self.name}-img.html'
         # Récupération d'un tableau contenant toutes les balises img
         tabImg = response.xpath(f"//img").getall()
-        # Ecriture du tableau dans le fichier "file-img.html"
-        writeStrInFile(filename, tabImg)
-        # Ecriture dans les logs que la page a bien été sauvegarder
-        self.log(f'File {filename} saved')
+        # split des lignes
+        if len(tabImg) != 0:
+            tab = tabImg[0].split("\n")
+            # suppression des commentaires
+            clean = self.removeComments(tab)
+            # Ecriture du tableau dans le fichier "file-img.html"
+            writeStrInFile(filename, clean)
+            # Ecriture dans les logs que la page a bien été sauvegarder
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise div
         filename = f'{self.name}-div.html'
         tabDiv = response.xpath(f"//div").getall()
-        writeStrInFile(filename, tabDiv)
-        self.log(f'File {filename} saved')
+        if len(tabDiv) != 0:
+            tab = tabDiv[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise p
         filename = f'{self.name}-p.html'
         tabP = response.xpath(f"//p").getall()
-        writeStrInFile(filename, tabP)
-        self.log(f'File {filename} saved')
+        if len(tabP) != 0:
+            tab = tabP[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise form
         filename = f'{self.name}-form.html'
         tabForm = response.xpath(f"//form").getall()
-        writeStrInFile(filename, tabForm)
-        self.log(f'File {filename} saved')
+        if len(tabForm) != 0:
+            tab = tabForm[0].split("\n")
+            clean = self.removeComments(tab) 
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise table
         filename = f'{self.name}-table.html'
         tabTable = response.xpath(f"//table").getall()
-        writeStrInFile(filename, tabTable)
-        self.log(f'File {filename} saved')
+        if len(tabTable) != 0:
+            tab = tabTable[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise ul
         filename = f'{self.name}-ul.html'
         tabUl = response.xpath(f"//ul").getall()
-        writeStrInFile(filename, tabUl)
-        self.log(f'File {filename} saved')
+        if len(tabUl) != 0:
+            tab = tabUl[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise input
         filename = f'{self.name}-input.html'
         tabInput = response.xpath(f"//input").getall()
-        writeStrInFile(filename, tabInput)
-        self.log(f'File {filename} saved')
+        if len(tabInput) != 0:
+            tab = tabInput[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise label
         filename = f'{self.name}-label.html'
         tabLabel = response.xpath(f"//label").getall()
-        writeStrInFile(filename, tabLabel)
-        self.log(f'File {filename} saved')
+        if len(tabLabel) != 0:
+            tab = tabLabel[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise span
         filename = f'{self.name}-span.html'
         tabSpan = response.xpath(f"//span").getall()
-        writeStrInFile(filename, tabSpan)
-        self.log(f'File {filename} saved')
+        if len(tabSpan) != 0:
+            tab = tabSpan[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
         # Même processus qu'au dessus pour la balise button
         filename = f'{self.name}-button.html'
         tabButton = response.xpath(f"//button").getall()
-        writeStrInFile(filename, tabButton)
-        self.log(f'File {filename} saved')
+        if len(tabButton) != 0:
+            tab = tabButton[0].split("\n")
+            clean = self.removeComments(tab)
+            writeStrInFile(filename, clean)
+            self.log(f'File {filename} saved')
 
 # class tabiSpider(scrapy.Spider):
 #     name = "tabi"
